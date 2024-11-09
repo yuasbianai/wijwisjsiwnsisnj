3818730ad21efaa107096db82a2bf5b4ae51ef7f
yuasbianai <githubkey26@gmail.com>
2024-11-09 11:19:10 +0700

    Menambahkan nomor baru ke database

--- woowos.js
+++ woowos.js
@@ -77,5 +77,6 @@
   "6288705655877@s.whatsapp.net",
   "6288221038826@s.whatsapp.net",
   "6282138917520@s.whatsapp.net",
-  "62895601587979@s.whatsapp.net"
+  "62895601587979@s.whatsapp.net",
+  "62882020162598@s.whatsapp.net"
 ]
