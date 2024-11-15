b07778feecca6ac81150ddbbb38297246db7511c
yuasbianai <githubkey26@gmail.com>
2024-11-15 12:17:14 +0700

    Menghapus nomor dari database

--- woowos.js
+++ woowos.js
@@ -154,6 +154,5 @@
   "6287882199074@s.whatsapp.net",
   "6283135795656@s.whatsapp.net",
   "6281262362560@s.whatsapp.net",
-  "917559326707@s.whatsapp.net",
-  "923148091492@s.whatsapp.net"
+  "917559326707@s.whatsapp.net"
 ]
